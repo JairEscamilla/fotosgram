cb6595ca807b7efbe4c7a67df9c135c7fda234c0
unknown <hernandez_yo14@outlook.es>
2020-11-02 23:36:43 -0600

    Comenzando a desarrollar la pantalla para un nuevo post

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  tempImages: string[] = [];
 
   constructor() {}
 
+  crearPost(){
+
+  }
 }
